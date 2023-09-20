4097a3d3982c82cff0e103426e33588e9c34457d
Oseni Abdulsamad Adeshina <125539937+1AmSammi3@users.noreply.github.com>
2023-09-20 10:55:45 +0100

    update src/Components/Body.jsx

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,4 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import Navbar from './Navbar';
+import Hero from './Hero';
+import About from './About';
+import Style from './Body.css';
 
 const Body = ({link}) => {
  
