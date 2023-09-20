7243bf78b1ccb09a3c8b15b1c858bd5c49b37dc8
Sammie <osenisamad2004@gmail.com>
2023-09-20 10:50:05 +0100

    Moved all components to a seperate folder

---
+++ src/Components/Body.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+
+const Body = ({link}) => {
+ 
+  return (
+  <div>
+    <Navbar />
+    <Hero />
+    <About />
+  </div>
+  );
+}
+
+export default Body;
