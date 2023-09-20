6a79cc382a0b0ce0f2e897fad9bdc5913b2b0c4d
Oseni Abdulsamad Adeshina <125539937+1AmSammi3@users.noreply.github.com>
2023-09-20 12:02:07 +0100

    update src/Components/Navbar.jsx

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Style from './Navbar.css';
+import Bot from './Bot';
+
+const Navbar = () => {
+  return(
+    <div>
+      <div>
+        <Bot />
+        <span>Logo Header</span>
+      </div>
+    </div>  
+  );
+}
+
+export default Navbar;
