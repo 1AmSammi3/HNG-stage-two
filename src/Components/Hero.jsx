f46142f2fd63db1f7f8031a7540a5db37ece8af6
Oseni Abdulsamad Adeshina <125539937+1AmSammi3@users.noreply.github.com>
2023-09-21 15:45:22 +0100

    update src/Components/Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,10 +4,13 @@ import Style from "./Hero.css";
 const Hero = () => {
   return(
     <div className="container">
-      <img src="src/Components/Assets/john.jpg" alt="A picture of john wick"/>
-      
+      <h1>This is a cool website</h1>
+      <h3>Wanna know more?<br>Click the link below</h3>
+      <button>Explore</button>
     </div>
   );
 }
 
 export default Hero
+
+/*<img src="src/Components/Assets/john.jpg" alt="A picture of john wick"/>*/
