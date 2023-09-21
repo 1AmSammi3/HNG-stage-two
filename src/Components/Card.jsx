6fd04dc4931c5b0893719a54304cb2de6de5c1bf
Oseni Abdulsamad Adeshina <125539937+1AmSammi3@users.noreply.github.com>
2023-09-21 19:22:15 +0100

    update src/Components/Card.jsx

---
+++ src/Components/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Style from "./Card.css";
+
+const Card = () => {
+  return(
+    <div>
+      <img src="./src/Components/Assets/manager.jpg"/>
+    </div>
+  );
+}
+
+export default Card;
