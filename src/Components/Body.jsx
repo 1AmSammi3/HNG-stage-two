159edc851a276ec143a18f4f5d6e15e2e3caefe7
Oseni Abdulsamad Adeshina <125539937+1AmSammi3@users.noreply.github.com>
2023-09-21 20:55:12 +0100

    update src/Components/Body.jsx

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -10,7 +10,7 @@ const Body = ({link}) => {
   <div>
     <Navbar />
     <Hero />
-    <div>
+    <div className="CardView">
       <Card img="./src/Components/Assets/manager.jpg"/>
       <Card img="./src/Components/Assets/CEO.png"/>
       <Card img="./src/Components/Assets/Founder.jpg"/>
