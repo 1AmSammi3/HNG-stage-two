550e6b8a565700153505d9c6684fa742021a9af1
Oseni Abdulsamad Adeshina <125539937+1AmSammi3@users.noreply.github.com>
2023-09-23 15:32:56 +0100

    update src/Components/Card.jsx

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,7 +4,7 @@ import Style from "./Card.css";
 const Card = (props) => {
   return(
     <div className="cardAll">
-      <img src={props.img}/>
+      <img src={`./src/Components/Assets/${props.img}`}/>
     </div>
   );
 }
