8bdf14d592b7adc50aada6e3ebcd3f9a732d0eb1
Oseni Abdulsamad Adeshina <125539937+1AmSammi3@users.noreply.github.com>
2023-09-24 18:34:01 +0100

    update src/Components/Body.jsx

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -7,15 +7,14 @@ import ImageData from './imagesData';
 
 const Body = ({link}) => {
  
+ const images = imagesData.map(image => {return <Card img={image.img} />});
+ 
   return (
   <div className="all">
     <Navbar />
     <Hero />
     <div className="cardView">
-      <Card img="manager.jpg"/>
-      <Card img="CEO.png"/>
-      <Card img="Founder.jpg"/>
-      <Card img="Director.jpg"/>
+      {images}
     </div>
   </div>
   );
