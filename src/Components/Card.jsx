2b51f27c04b68990b21f77a8e745a5198bea0e83
Oseni Abdulsamad Adeshina <125539937+1AmSammi3@users.noreply.github.com>
2023-09-25 18:16:15 +0100

    update src/Components/Card.jsx

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Style from "./Card.css";
 
-const Card = ({img}) => {
+const Card = (props) => {
   return(
     <div className="cardAll">
-      <img src={`./public/Assets/${img}`}/>
+      <img src={`./public/Assets/${props.img}`}/>
+      <h1>{props.post}<h1/>
+      <h2>{props.contact}<h2/>
     </div>
   );
 }
