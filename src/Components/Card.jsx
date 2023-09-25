cb7196a3bb1d1b3af928a9c4ab1f4b6cfb4b2e88
Oseni Abdulsamad Adeshina <125539937+1AmSammi3@users.noreply.github.com>
2023-09-25 18:25:44 +0100

    update src/Components/Card.jsx

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -5,8 +5,8 @@ const Card = (props) => {
   return(
     <div className="cardAll">
       <img src={`./public/Assets/${props.img}`}/>
-      <h1>{props.post}<h1/>
-      <h2>{props.contact}<h2/>
+      <h1>{props.post}</h1>
+      <h2>{props.contact}</h2>
     </div>
   );
 }
